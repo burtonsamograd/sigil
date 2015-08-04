7210ce3bca62e8f544e0a5d35cd40795cef39b70
Burton Samograd <burton.samograd@gmail.com>
2015-08-03 23:10:14 -0600

    Added --eval, --pseval.

--- ps2js.lisp
+++ ps2js.lisp
@@ -15,17 +15,37 @@
   (do
    ((form (read f nil) (read f nil)))
    ((not form))
-    (format *error-output*  "// ~A~%" form)
+    (format t  "/* ~A */~%" form)
     (format t "~A~%" (ps:ps* form))))
+
+(defmacro while (test &body body)
+  `(loop
+      (when (not ,test)
+        (return))
+      ,@body))
 
 (defun main (argv)
   (if (cdr argv)
-      (dolist (file (cdr argv))
-        (with-open-file (f file)
-          (handler-bind
-              ((error
-                (lambda (e) 
-                  (format *error-output* "~A~%" e)
-                  (sb-ext:exit :code 1))))
-            (ps2js f))))
+      (progn
+        (pop argv)
+        (while argv
+          (let ((arg (pop argv)))
+            (cond 
+              ((string= arg "--eval")
+               (let ((code (pop argv)))
+                 (format t "/* --eval ~A~% */" (read-from-string code))
+                 (in-package :ps)
+                 (eval (read-from-string code))))
+              ((string= arg "--pseval")
+               (let ((code (pop argv)))
+                 (format t "/* --pseval ~A~% */" (read-from-string code))
+                 (ps:ps* (read-from-string code))))
+              (t
+               (with-open-file (f arg)
+                 (handler-bind
+                     ((error
+                       (lambda (e) 
+                         (format *error-output* "~A~%" e)
+                         (sb-ext:exit :code 1))))
+                   (ps2js f))))))))
       (ps2js *standard-input*)))
