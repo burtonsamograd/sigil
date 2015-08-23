c66f35ed0c524162f1dfc915856f712766b24427
Burton Samograd <burton.samograd@gmail.com>
2015-08-22 22:44:54 -0600

    Make real REPL using node backend.

--- sigil.lisp
+++ sigil.lisp
@@ -36,15 +36,29 @@
       ,@body))
 
 (defun repl ()
-  (loop
-     (format *error-output* "> ")
-     (force-output *error-output*)
-     (handler-case
-         (let ((form (read)))
-           (format t "~A~%" (ps:ps* form)))
-       (sb-sys:interactive-interrupt () (sb-ext:exit))
-       (end-of-file () (sb-ext:exit))
-       )))
+  (let* ((node (run-program "node" '("-i") :search t :input :stream :output :stream :wait nil))
+         (node-input (process-input node))
+         (node-output (process-output node)))
+    (loop
+       (format *error-output* "> ")
+       (force-output *error-output*)
+       (read-char node-output) ; eat initial prompt
+       (handler-case
+           (let ((form (read)))
+             (format node-input "~A~%" (ps:ps* form))
+             (force-output node-input)
+             (loop
+                (let ((c (read-char node-output)))
+                  (when (and (char= #\Newline c)
+                             (char= #\> (peek-char nil node-output)))
+                    (read-char node-output)
+                    (fresh-line)
+                    (return))
+                  (princ c)
+                  (force-output))))
+         (sb-sys:interactive-interrupt () (sb-ext:exit))
+         (end-of-file () (sb-ext:exit))
+         ))))
 
 (defun main (argv)
   (push (probe-file ".") *include-paths*)
